e63485c7c732d3376a47fe93291c33959b17dfe0
Richie <richimf@hotmail.com>
2016-02-03 14:02:00 -0600

    Project created
    Creamos el proyecto

---
+++ LoginiOS/LoginiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginiOS
+//
+//  Created by Sistemas on 03/02/16.
+//  Copyright © 2016 com.richie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
